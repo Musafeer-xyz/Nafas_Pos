929d9e61e498e4d29bf2ad482e53315bf431cfec
Musafeer-xyz <tanjimhassan666@gmail.com>
2026-05-03 07:50:53 +0600

    fix revenue calc, bottle deduction, assistant product permission

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/productController');
-const { auth, adminOnly } = require('../middleware/auth');
+const { auth, adminOnly, can } = require('../middleware/auth');
 
 router.get('/', auth, ctrl.getAll);
 router.get('/:id', auth, ctrl.getOne);
-router.post('/', auth, adminOnly, ctrl.create);
-router.put('/:id', auth, adminOnly, ctrl.update);
-router.patch('/:id/stock', auth, adminOnly, ctrl.adjustStock);
+router.post('/', auth, can('addProducts'), ctrl.create);
+router.put('/:id', auth, can('addProducts'), ctrl.update);
+router.patch('/:id/stock', auth, can('addProducts'), ctrl.adjustStock);
 router.delete('/:id', auth, adminOnly, ctrl.remove);
 
 module.exports = router;
